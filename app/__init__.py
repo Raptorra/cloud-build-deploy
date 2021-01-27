2f41d43736a8715ee0d97052d053d1e9e699d154
Léo <leo.delwaulle@gmail.com>
2021-01-27 13:51:24 +0100

    Change output to json

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_route():
-    return "Hello World !"
+    return jsonify({
+        "message": "Hello World !"
+    }), 200
 
 
 if __name__ == "__main__":
